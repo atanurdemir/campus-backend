c53e721cb3fe8d690c75ec4db255fdd38737fb5f
Atanur <a.demir@azerion.com>
2022-01-23 02:07:17 +0300

    GetUser, GetMe, UpdateUser, UpdateStudent Implemented

--- handlers/user.js
+++ handlers/user.js
@@ -4,66 +4,19 @@ AWS.config.update({
 });
 require("dotenv").config();
 const uuid4 = require("uuid4");
-const crypto = require("crypto");
 const moment = require("moment");
 const jwt = require("jsonwebtoken");
 const generateResponse = require("./response");
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
-exports.lambdaAuthorizer = (event, context, callback) => {
-  var headers = event.headers;
-  var queryStringParameters = event.queryStringParameters;
-  var pathParameters = event.pathParameters;
-  var stageVariables = event.stageVariables;
-
-  // Parse the input for the parameter values
-  var tmp = event.methodArn.split(":");
-  var apiGatewayArnTmp = tmp[5].split("/");
-  var awsAccountId = tmp[4];
-  var region = tmp[3];
-  var restApiId = apiGatewayArnTmp[0];
-  var stage = apiGatewayArnTmp[1];
-  var method = apiGatewayArnTmp[2];
-  var resource = "/";
-  if (apiGatewayArnTmp[3]) {
-    resource += apiGatewayArnTmp[3];
-  }
-  var authResponse = {};
-  var condition = {};
-  condition.IpAddress = {};
-
-  if (headers.Authorization !== "") {
-    let decryptedToken = {
-      userId: "null",
-      email: "null",
-    };
-    if (headers.Authorization !== "public") {
-      try {
-        const data = jwt.verify(
-          headers.Authorization,
-          process.env.ACCESS_TOKEN_SECRET
-        );
-        decryptedToken = data;
-      } catch (e) {
-        callback("Unauthorized");
-      }
-    } else {
-      //TODO: put here public urls
-    }
-    callback(null, generateAllow("me", event.methodArn, decryptedToken));
-  } else {
-    callback("Unauthorized");
-  }
-};
-
-var generatePolicy = function (principalId, effect, resource, decryptedToken) {
-  var authResponse = {};
+function generatePolicy(principalId, effect, resource, decryptedToken) {
+  const authResponse = {};
   authResponse.principalId = principalId;
   if (effect && resource) {
-    var policyDocument = {};
+    const policyDocument = {};
     policyDocument.Version = "2012-10-17";
     policyDocument.Statement = [];
-    var statementOne = {};
+    const statementOne = {};
     statementOne.Action = "execute-api:Invoke";
     statementOne.Effect = effect;
     statementOne.Resource = resource;
@@ -72,22 +25,49 @@ var generatePolicy = function (principalId, effect, resource, decryptedToken) {
   }
   authResponse.context = {
     userId: decryptedToken.userId,
-    email: decryptedToken.email,
+    username: decryptedToken.username,
+    azerionConnectId: decryptedToken.azerionConnectId,
   };
   return authResponse;
-};
+}
 
-var generateAllow = function (principalId, resource, decryptedToken) {
+function generateAllow(principalId, resource, decryptedToken) {
   return generatePolicy(principalId, "Allow", resource, decryptedToken);
-};
+}
 
-var generateDeny = function (principalId, resource) {
-  return generatePolicy(principalId, "Deny", resource);
+exports.lambdaAuthorizer = (event, context, callback) => {
+  const { headers } = event;
+  const condition = {};
+  condition.IpAddress = {};
+
+  if (headers.Authorization !== "") {
+    let decryptedToken = {
+      userId: null,
+      email: null,
+    };
+    if (headers.Authorization !== "public") {
+      try {
+        const data = JWTService.verifyToken(headers.Authorization);
+        decryptedToken = data;
+      } catch (e) {
+        console.log("headers", headers, process.env.ACCESS_TOKEN_SECRET);
+        callback("Unauthorized", headers.Authorization);
+      }
+    } else {
+      // TODO: put here public urls
+    }
+    // elastic search sample data output
+    console.log(event);
+    console.log(decryptedToken);
+    callback(null, generateAllow("me", event.methodArn, decryptedToken));
+  } else {
+    console.log("headers", headers);
+    callback("Unauthorized");
+  }
 };
 
 exports.signup = async (event) => {
   let response;
-  const body = JSON.parse(event.body);
   const {
     idNo,
     email,
@@ -101,7 +81,7 @@ exports.signup = async (event) => {
     highSchool,
     personalInfo,
     universityExam,
-  } = body;
+  } = JSON.parse(event.body);
 
   const checkParams = {
     TableName: "Users",
@@ -120,13 +100,15 @@ exports.signup = async (event) => {
       email,
       family,
       health,
-      firstName,
+      password,
       lastName,
+      firstName,
       emergency,
       highSchool,
       personalInfo,
       universityExam,
       userId: uuid4(),
+      role: "Student",
       createAt: moment().utc().format(),
     },
   };
@@ -140,16 +122,6 @@ exports.signup = async (event) => {
         )
         .send());
     }
-    const IV = crypto.randomBytes(8).toString("hex");
-    const cipher = crypto.createCipheriv(
-      "aes-256-cbc",
-      process.env.ENCRYPTION_KEY,
-      IV
-    );
-    const encrypted =
-      cipher.update(password, "utf8", "base64") + cipher.final("base64");
-    params.Item.password = encrypted;
-    params.Item.salt = IV;
     await documentClient.put(params).promise();
     response = generateResponse.send();
   } catch (error) {
@@ -165,8 +137,7 @@ exports.signup = async (event) => {
 
 exports.signin = async (event) => {
   let response;
-  const body = JSON.parse(event.body);
-  const { email, password } = body;
+  const { email, password } = JSON.parse(event.body);
 
   const checkParams = {
     TableName: "Users",
@@ -180,25 +151,19 @@ exports.signin = async (event) => {
   try {
     const checkUser = await documentClient.query(checkParams).promise();
     if (checkUser.Items[0]) {
-      let decipher = crypto.createDecipheriv(
-        "aes-256-cbc",
-        process.env.ENCRYPTION_KEY,
-        checkUser.Items[0].salt
-      );
-      let decrypted =
-        decipher.update(checkUser.Items[0].password, "base64", "utf8") +
-        decipher.final("utf8");
       const tokenObj = {
         userId: checkUser.Items[0].userId,
         email: checkUser.Items[0].email,
+        role: checkUser.Items[0].role,
       };
       const token = jwt.sign(tokenObj, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "30 days",
       });
-      return decrypted === password
+      return checkUser.Items[0].password === password
         ? generateResponse.send({
             userId: checkUser.Items[0].userId,
             email: checkUser.Items[0].email,
+            role: checkUser.Items[0].role,
             token: token,
           })
         : generateResponse.message("Incorrect password!").send();
@@ -211,16 +176,16 @@ exports.signin = async (event) => {
     console.log(error);
     response = generateResponse
       .message(
-        "User could not be signin due to connection issues to the Azerion TrackingApp Services!"
+        "User could not be signin due to connection issues to the Campus Services!"
       )
       .send();
   }
   return response;
 };
 
-exports.getUser = async (event) => {
+exports.get = async (event) => {
   let response;
-  const userId = event.requestContext.authorizer.userId;
+  const { userId } = event.pathParameters;
   const params = {
     TableName: "Users",
     Key: {
@@ -234,7 +199,119 @@ exports.getUser = async (event) => {
     console.log(error);
     response = generateResponse
       .message(
-        "User could not be accessed due to connection issues to the Azerion TrackingApp Services!"
+        "User could not be accessed due to connection issues to the Campus Services!"
+      )
+      .send();
+  }
+  return response;
+};
+
+exports.getMe = async (event) => {
+  let response;
+  const { userId } = event.requestContext.authorizer;
+  const params = {
+    TableName: "Users",
+    Key: {
+      userId,
+    },
+  };
+  try {
+    const res = await documentClient.get(params).promise();
+    response = generateResponse.send({ data: res.Item });
+  } catch (error) {
+    console.log(error);
+    response = generateResponse
+      .message(
+        "User could not be accessed due to connection issues to the Campus Services!"
+      )
+      .send();
+  }
+  return response;
+};
+
+exports.updateUser = async (event) => {
+  let response;
+  const { userId } = event.requestContext.authorizer;
+  const { email, password } = JSON.parse(event.body);
+  const updateAt = new Date().toISOString().split("T")[0];
+
+  const params = {
+    TableName: "User",
+    Key: {
+      userId,
+    },
+    UpdateExpression: "set email = :mail, password = :pass, updateAt = :uptd",
+    ExpressionAttributeValues: {
+      ":mail": email,
+      ":uptd": updateAt,
+      ":pass": password,
+    },
+    ReturnValues: "UPDATED_NEW",
+  };
+  try {
+    await documentClient.update(params).promise();
+    response = generateResponse.send();
+  } catch (error) {
+    console.log(error);
+    response = generateResponse
+      .message(
+        "User could not be updated due to connection issues to the Campus Services!"
+      )
+      .send();
+  }
+  return response;
+};
+
+exports.updateStudent = async (event) => {
+  let response;
+  const { userId } = event.pathParameters;
+  //TODO: const { adminId } = event.requestContext.authorizer;
+  const {
+    idNo,
+    email,
+    place,
+    family,
+    health,
+    lastName,
+    emergency,
+    firstName,
+    highSchool,
+    personalInfo,
+    universityExam,
+  } = JSON.parse(event.body);
+  const updateAt = new Date().toISOString().split("T")[0];
+
+  const params = {
+    TableName: "User",
+    Key: {
+      userId,
+    },
+    UpdateExpression:
+      "set idNo = :idno, email = :mail, place = :plac, family = :faml, health = :heal, lastName = :last, emergency = :emer, firstName = :frst, highSchool = :high, personalInfo = :prsn, universityExam = :exam, updateAt = :uptd",
+    ExpressionAttributeValues: {
+      ":idno": idNo,
+      ":mail": email,
+      ":plac": place,
+      ":faml": family,
+      ":heal": health,
+      ":uptd": updateAt,
+      ":last": lastName,
+      ":emer": emergency,
+      ":frst": firstName,
+      ":high": highSchool,
+      ":prsn": personalInfo,
+      ":exam": universityExam,
+    },
+    ReturnValues: "UPDATED_NEW",
+  };
+  try {
+    await documentClient.update(params).promise();
+    response = generateResponse.send();
+  } catch (error) {
+    console.log(error);
+    response = generateResponse
+      .message(
+        "User could not be updated due to connection issues to the Campus Services!"
       )
       .send();
   }
