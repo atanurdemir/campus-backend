ab667403d6378fd5faf992c44b37e5cf5b0da8c6
Atanur <a.demir@azerion.com>
2022-01-25 02:28:38 +0300

    Admin.js created

---
+++ handlers/admin.js
@@ -0,0 +1,163 @@
+const AWS = require("aws-sdk");
+AWS.config.update({
+  region: "eu-west-1",
+});
+require("dotenv").config();
+const uuid4 = require("uuid4");
+const moment = require("moment");
+const generateResponse = require("./response");
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+exports.signup = async (event) => {
+  let response;
+  const zr = Object.create(generateResponse);
+  const {
+    uni,
+    idNo,
+    email,
+    place,
+    family,
+    health,
+    teacher,
+    lastName,
+    emergency,
+    firstName,
+    highSchool,
+    personalInfo,
+    universityExam,
+  } = JSON.parse(event.body);
+
+  const checkParams = {
+    TableName: "Users",
+    IndexName: "emailIndex",
+    KeyConditionExpression: "email = :e",
+    ExpressionAttributeValues: {
+      ":e": email,
+    },
+  };
+
+  const params = {
+    TableName: "Users",
+    Item: {
+      uni,
+      idNo,
+      place,
+      email,
+      family,
+      health,
+      teacher,
+      lastName,
+      firstName,
+      emergency,
+      highSchool,
+      personalInfo,
+      universityExam,
+      password: idNo,
+      userId: uuid4(),
+      role: "Student",
+      createAt: moment().utc().format(),
+    },
+  };
+
+  try {
+    const checkUser = await documentClient.query(checkParams).promise();
+    if (checkUser.Items.length) {
+      response = zr
+        .message(
+          "The e-mail you entered is already in use, please select an alternative."
+        )
+        .send();
+    }
+    await documentClient.put(params).promise();
+    response = zr.send();
+  } catch (error) {
+    console.log(error);
+    response = zr
+      .message(
+        "User could not be signup due to connection issues to the Campus Services."
+      )
+      .send();
+  }
+  return response;
+};
+
+exports.getStudent = async (event) => {
+  let response;
+  const { email } = event.pathParameters;
+  const params = {
+    TableName: "Users",
+    IndexName: "emailIndex",
+    KeyConditionExpression: "email = :e",
+    ExpressionAttributeValues: {
+      ":e": email,
+    },
+  };
+  try {
+    const res = await documentClient.query(params).promise();
+    response = generateResponse.send({ data: res.Items });
+  } catch (error) {
+    console.log(error);
+    response = generateResponse
+      .message(
+        "User could not be accessed due to connection issues to the Campus Services!"
+      )
+      .send();
+  }
+  return response;
+};
+
+exports.updateStudent = async (event) => {
+  let response;
+  const { userId } = event.pathParameters;
+  //TODO: const { adminId } = event.requestContext.authorizer;
+  const {
+    idNo,
+    email,
+    place,
+    family,
+    health,
+    lastName,
+    emergency,
+    firstName,
+    highSchool,
+    personalInfo,
+    universityExam,
+  } = JSON.parse(event.body);
+  const updateAt = new Date().toISOString().split("T")[0];
+
+  const params = {
+    TableName: "User",
+    Key: {
+      userId,
+    },
+    UpdateExpression:
+      "set idNo = :idno, email = :mail, place = :plac, family = :faml, health = :heal, lastName = :last, emergency = :emer, firstName = :frst, highSchool = :high, personalInfo = :prsn, universityExam = :exam, updateAt = :uptd",
+    ExpressionAttributeValues: {
+      ":idno": idNo,
+      ":mail": email,
+      ":plac": place,
+      ":faml": family,
+      ":heal": health,
+      ":uptd": updateAt,
+      ":last": lastName,
+      ":emer": emergency,
+      ":frst": firstName,
+      ":high": highSchool,
+      ":prsn": personalInfo,
+      ":exam": universityExam,
+    },
+    ReturnValues: "UPDATED_NEW",
+  };
+  try {
+    await documentClient.update(params).promise();
+    response = generateResponse.send();
+  } catch (error) {
+    console.log(error);
+    response = generateResponse
+      .message(
+        "User could not be updated due to connection issues to the Campus Services!"
+      )
+      .send();
+  }
+  return response;
+};
--- handlers/user.js
+++ handlers/user.js
@@ -80,79 +80,6 @@ var generateAllow = function (principalId, resource, decryptedToken) {
   return generatePolicy(principalId, "Allow", resource, decryptedToken);
 };
 
-exports.signup = async (event) => {
-  let response;
-  const zr = Object.create(generateResponse);
-  const {
-    uni,
-    idNo,
-    email,
-    place,
-    family,
-    health,
-    teacher,
-    lastName,
-    emergency,
-    firstName,
-    highSchool,
-    personalInfo,
-    universityExam,
-  } = JSON.parse(event.body);
-
-  const checkParams = {
-    TableName: "Users",
-    IndexName: "emailIndex",
-    KeyConditionExpression: "email = :e",
-    ExpressionAttributeValues: {
-      ":e": email,
-    },
-  };
-
-  const params = {
-    TableName: "Users",
-    Item: {
-      uni,
-      idNo,
-      place,
-      email,
-      family,
-      health,
-      teacher,
-      lastName,
-      firstName,
-      emergency,
-      highSchool,
-      personalInfo,
-      universityExam,
-      password: idNo,
-      userId: uuid4(),
-      role: "Student",
-      createAt: moment().utc().format(),
-    },
-  };
-
-  try {
-    const checkUser = await documentClient.query(checkParams).promise();
-    if (checkUser.Items.length) {
-      response = zr
-        .message(
-          "The e-mail you entered is already in use, please select an alternative."
-        )
-        .send();
-    }
-    await documentClient.put(params).promise();
-    response = zr.send();
-  } catch (error) {
-    console.log(error);
-    response = zr
-      .message(
-        "User could not be signup due to connection issues to the Campus Services."
-      )
-      .send();
-  }
-  return response;
-};
-
 exports.signin = async (event) => {
   let response;
   const zr = Object.create(generateResponse);
@@ -166,10 +93,8 @@ exports.signin = async (event) => {
       ":e": email,
     },
   };
-
   try {
     const checkUser = await documentClient.query(checkParams).promise();
-
     if (checkUser.Items[0]) {
       if (checkUser.Items[0].password === password) {
         const tokenObj = {
@@ -198,31 +123,6 @@ exports.signin = async (event) => {
     response = zr
       .message(
         "User could not be signin due to connection issues to the Campus Services!"
-      )
-      .send();
-  }
-  return response;
-};
-
-exports.get = async (event) => {
-  let response;
-  const { email } = event.pathParameters;
-  const params = {
-    TableName: "Users",
-    IndexName: "emailIndex",
-    KeyConditionExpression: "email = :e",
-    ExpressionAttributeValues: {
-      ":e": email,
-    },
-  };
-  try {
-    const res = await documentClient.query(params).promise();
-    response = generateResponse.send({ data: res.Items });
-  } catch (error) {
-    console.log(error);
-    response = generateResponse
-      .message(
-        "User could not be accessed due to connection issues to the Campus Services!"
       )
       .send();
   }
@@ -268,62 +168,6 @@ exports.updateUser = async (event) => {
       ":mail": email,
       ":uptd": updateAt,
       ":pass": password,
-    },
-    ReturnValues: "UPDATED_NEW",
-  };
-  try {
-    await documentClient.update(params).promise();
-    response = generateResponse.send();
-  } catch (error) {
-    console.log(error);
-    response = generateResponse
-      .message(
-        "User could not be updated due to connection issues to the Campus Services!"
-      )
-      .send();
-  }
-  return response;
-};
-
-exports.updateStudent = async (event) => {
-  let response;
-  const { userId } = event.pathParameters;
-  //TODO: const { adminId } = event.requestContext.authorizer;
-  const {
-    idNo,
-    email,
-    place,
-    family,
-    health,
-    lastName,
-    emergency,
-    firstName,
-    highSchool,
-    personalInfo,
-    universityExam,
-  } = JSON.parse(event.body);
-  const updateAt = new Date().toISOString().split("T")[0];
-
-  const params = {
-    TableName: "User",
-    Key: {
-      userId,
-    },
-    UpdateExpression:
-      "set idNo = :idno, email = :mail, place = :plac, family = :faml, health = :heal, lastName = :last, emergency = :emer, firstName = :frst, highSchool = :high, personalInfo = :prsn, universityExam = :exam, updateAt = :uptd",
-    ExpressionAttributeValues: {
-      ":idno": idNo,
-      ":mail": email,
-      ":plac": place,
-      ":faml": family,
-      ":heal": health,
-      ":uptd": updateAt,
-      ":last": lastName,
-      ":emer": emergency,
-      ":frst": firstName,
-      ":high": highSchool,
-      ":prsn": personalInfo,
-      ":exam": universityExam,
     },
     ReturnValues: "UPDATED_NEW",
   };
