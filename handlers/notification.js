558edf103315c7d37c2d918d4b40d8e4f75fa2df
Atanur <a.demir@azerion.com>
2022-01-25 03:32:44 +0300

    Notification Implemented

---
+++ handlers/notification.js
@@ -0,0 +1,55 @@
+const AWS = require("aws-sdk");
+AWS.config.update({
+  region: "eu-west-1",
+});
+const moment = require("moment");
+const generateResponse = require("./response");
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+exports.add = async (event) => {
+  let response;
+  const zr = Object.create(generateResponse);
+  const { userId } = event.requestContext.authorizer;
+  const { title, message } = JSON.parse(event.body);
+  const params = {
+    TableName: "Notifications",
+    Item: {
+      userId,
+      title,
+      message,
+      createAt: moment().utc().format("YYYY-MM-DD"),
+    },
+  };
+  try {
+    await documentClient.put(params).promise();
+    response = zr.send();
+  } catch (error) {
+    console.log(JSON.stringify(error));
+    response = zr
+      .message(
+        "User could not be accessed due to connection issues to the Campus Services!"
+      )
+      .send();
+  }
+  return response;
+};
+
+exports.get = async () => {
+  let response;
+  const zr = Object.create(generateResponse);
+  const params = {
+    TableName: "Notifications",
+  };
+  try {
+    const res = await documentClient.scan(params).promise();
+    response = zr.send({ data: res.Items });
+  } catch (error) {
+    console.log(error);
+    response = zr
+      .message(
+        "User could not be accessed due to connection issues to the Campus Services!"
+      )
+      .send();
+  }
+  return response;
+};
