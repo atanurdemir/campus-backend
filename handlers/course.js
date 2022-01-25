3a2720883700bc05acf684631e28382677714cac
Atanur <a.demir@azerion.com>
2022-01-26 01:04:34 +0300

    removeCourse Implemented

--- handlers/course.js
+++ handlers/course.js
@@ -9,28 +9,42 @@ const documentClient = new AWS.DynamoDB.DocumentClient();
 
 exports.add = async (event) => {
   let response;
+  const zr = Object.create(generateResponse);
   const { userId } = event.requestContext.authorizer;
   const { courseName, courseCode, courseCredit, courseTeacher } = JSON.parse(
     event.body
   );
+  const checkParams = {
+    TableName: "Courses",
+    IndexName: "nameIndex",
+    KeyConditionExpression: "courseName = :nö",
+    ExpressionAttributeValues: {
+      ":nm": courseName,
+    },
+  };
   const params = {
     TableName: "Courses",
     Item: {
       userId,
-      courseId: uuid4(),
       courseName,
       courseCode,
       courseCredit,
       courseTeacher,
+      courseId: uuid4(),
       createAt: moment().utc().format("YYYY-MM-DD"),
     },
   };
   try {
-    await documentClient.put(params).promise();
-    response = generateResponse.send({});
+    const res = await documentClient.query(params).promise();
+    if (!res.Items.data) {
+      await documentClient.put(params).promise();
+      response = zr.send({});
+    } else {
+      response = zr.message("You've already taken this course!").send();
+    }
   } catch (error) {
-    console.log(JSON.stringify(error));
-    response = generateResponse
+    console.log(error);
+    response = zr
       .message(
         "User could not be accessed due to connection issues to the Campus Services!"
       )
@@ -46,6 +60,37 @@ exports.getById = async (event) => {
     TableName: "Courses",
     IndexName: "userIndex",
     KeyConditionExpression: "#userId = :ui",
+    ExpressionAttributeNames: {
+      "#userId": "userId",
+    },
+    ExpressionAttributeValues: {
+      ":ui": userId,
+    },
+  };
+  try {
+    const res = await documentClient.query(params).promise();
+    response = generateResponse.send({ data: res.Items });
+  } catch (error) {
+    console.log(error);
+    response = generateResponse
+      .message(
+        "User could not be accessed due to connection issues to the Campus Services!"
+      )
+      .send();
+  }
+  return response;
+};
+
+exports.remove = async (event) => {
+  let response;
+  const { courseId } = event.pathParameters;
+  const { userId } = event.requestContext.authorizer;
+  const params = {
+    TableName: "Courses",
+    Key: {
+      courseId,
+    },
+    ConditionExpression: "#userId = :ui",
     ExpressionAttributeNames: {
       "#userId": "userId",
     },
