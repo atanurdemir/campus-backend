501f8a6b8ce5e9070ff1c437a4ca08aa587aa121
Atanur <a.demir@azerion.com>
2022-01-26 19:09:58 +0300

    Reservation Fixed

--- handlers/cafeteria.js
+++ handlers/cafeteria.js
@@ -65,11 +65,11 @@ exports.addCron = async () => {
 
 exports.reserve = async (event) => {
   let response;
-  const { name } = JSON.parse(event.body);
+  const { cafeteriaId } = JSON.parse(event.body);
   const params = {
     TableName: "Cafeterias",
     Key: {
-      cafeteriaId: name + "?" + moment().utc().format("YYYY-MM-DD"),
+      cafeteriaId,
     },
     UpdateExpression: "set reserved = reserved + :num",
     ExpressionAttributeValues: {
@@ -106,6 +106,58 @@ exports.get = async () => {
   try {
     const res = await documentClient.query(params).promise();
     response = generateResponse.send({ data: res.Items });
+  } catch (error) {
+    console.log(error);
+    response = generateResponse
+      .message(
+        "User could not be accessed due to connection issues to the Campus Services!"
+      )
+      .send();
+  }
+  return response;
+};
+
+exports.addReservation = async (event) => {
+  let response;
+  const { cafeteriaId } = JSON.parse(event.body);
+  const { userId } = event.requestContext.authorizer;
+  const params = {
+    TableName: "Reservations",
+    Item: {
+      cafeteriaId,
+      createAt: moment().utc().format("YYYY-MM-DD"),
+      userId: userId + "?" + moment().utc().format("YYYY-MM-DD"),
+    },
+  };
+  try {
+    await documentClient.put(params).promise();
+    response = generateResponse.send({});
+  } catch (error) {
+    console.log(JSON.stringify(error));
+    response = generateResponse
+      .message(
+        "User could not be accessed due to connection issues to the Campus Services!"
+      )
+      .send();
+  }
+  return response;
+};
+
+exports.getReservation = async (event) => {
+  let response;
+  const { userId } = event.requestContext.authorizer;
+  const { cafeteriaId } = event.pathParameters;
+  const params = {
+    TableName: "Reservations",
+    Key: { userId: userId + "?" + moment().utc().format("YYYY-MM-DD") },
+    FilterExpression: "cafeteriaId = :ci",
+    ExpressionAttributeValues: {
+      ":ci": cafeteriaId,
+    },
+  };
+  try {
+    const res = await documentClient.get(params).promise();
+    response = generateResponse.send({ data: res.Item });
   } catch (error) {
     console.log(error);
     response = generateResponse
